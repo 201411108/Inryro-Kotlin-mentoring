08fe335fa0911670bb6c285adeb1ab5a027c3fff
sally5131 <yeyeah5131@naver.com>
2020-01-16 13:20:38 +0900

    문제1 2577풀었음

---
+++ yejin/src/2577.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+fun main(args:Array<String>) : Unit{
+    val scan =Scanner(System.`in`)
+    val A=scan.nextInt()
+    val B=scan.nextInt()
+    val C=scan.nextInt()
+    var result=A*B*C
+    var arrEx=arrayOf(0,0,0,0,0,0,0,0,0)
+    while(result>0){
+        arrEx[result%10]++
+        result=result/10
+
+     }
+    for(a in 0..9){
+        println(arrEx[a])
+    }
+}
