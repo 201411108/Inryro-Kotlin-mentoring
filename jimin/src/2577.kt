533341c8976ca2bf7bd4ff5e22fba291b56efb1d
jiminAN <als398@naver.com>
2020-01-16 13:06:40 +0900

    gg

--- jimin/src/2577.kt
+++ jimin/src/2577.kt
@@ -11,18 +11,7 @@ fun main(){
 
     while(result != 0){
         tmp = result % 10
-        when(tmp){
-            0->arr[0]++
-            1->arr[1]++
-            2->arr[2]++
-            3->arr[3]++
-            4->arr[4]++
-            5->arr[5]++
-            6->arr[6]++
-            7->arr[7]++
-            8->arr[8]++
-            9->arr[9]++
-        }
+        arr[tmp]++
         result /= 10
     }
     for(i in 0..9){
